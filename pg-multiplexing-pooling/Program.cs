67534c20952fb365f70a36f3cc546bf1831de723
akilin <kilinandrei@gmail.com>
2020-11-16 01:11:48 +0200

    repro of multiplexing issue

---
+++ pg-multiplexing-pooling/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Collections.Generic;
+
+namespace pg_multiplexing_pooling
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using var ctx = new AppContext();
+            ctx.Database.EnsureDeleted();
+            ctx.Database.Migrate();
+        }
+    }
+
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                new NpgsqlConnectionStringBuilder
+                {
+                    Host = "localhost",
+                    Database = "test",
+                    Username = "guest",
+                    Password = "pwd",
+                    Pooling = true,
+                    Multiplexing = true
+                }.ToString());
+    }
+}
