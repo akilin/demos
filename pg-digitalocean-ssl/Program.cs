0a3059265a4e7bf22d9232409dba098eb3d989a1
akilin <kilinandrei@gmail.com>
2021-07-21 19:52:01 +0300

    simplified ssl demo

--- pg-digitalocean-ssl/Program.cs
+++ pg-digitalocean-ssl/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Npgsql;
+﻿using Npgsql;
 
 namespace pg_digitalocean_ssl
 {
@@ -7,25 +6,17 @@ namespace pg_digitalocean_ssl
     {
         static void Main(string[] args)
         {
-            using var ctx = new AppContext();
-            ctx.Database.OpenConnection();
-            System.Console.WriteLine("done.");
+            var conStr = new NpgsqlConnectionStringBuilder
+            {
+                Host = "db-postgresql-fra1-85405-do-user-9543433-0.b.db.ondigitalocean.com",
+                Database = "postgres",
+                Username = "doadmin",
+                Password = "zyk19s2agqce2czf",
+                Port = 25060,
+                SslMode = SslMode.Require,
+                RootCertificate = "ca-certificate.crt"
+            }.ConnectionString;
+            new NpgsqlConnection(conStr).Open();
         }
-    }
-
-    public class AppContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql(
-                new NpgsqlConnectionStringBuilder
-                {
-                    Host = "db-postgresql-fra1-85405-do-user-9543433-0.b.db.ondigitalocean.com",
-                    Database = "postgres",
-                    Username = "doadmin",
-                    Password = "zyk19s2agqce2czf",
-                    Port = 25060,
-                    SslMode = SslMode.Require,
-                    RootCertificate = "ca-certificate.crt"
-                }.ConnectionString);
     }
 }
