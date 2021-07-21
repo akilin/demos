d9c7fe8a2f7b7c566eae62e6d7fd300a091649bd
akilin <kilinandrei@gmail.com>
2021-07-21 19:43:06 +0300

    ssl-portgres-selfsigned

---
+++ pg-digitalocean-ssl/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace pg_digitalocean_ssl
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using var ctx = new AppContext();
+            ctx.Database.OpenConnection();
+            System.Console.WriteLine("done.");
+        }
+    }
+
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                new NpgsqlConnectionStringBuilder
+                {
+                    Host = "db-postgresql-fra1-85405-do-user-9543433-0.b.db.ondigitalocean.com",
+                    Database = "postgres",
+                    Username = "doadmin",
+                    Password = "",
+                    Port = 25060,
+                    SslMode = SslMode.Require,
+                    RootCertificate = "ca-certificate.crt",
+                    TrustServerCertificate = true
+                }.ConnectionString);
+    }
+}
