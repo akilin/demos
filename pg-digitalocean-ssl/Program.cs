a793e470938d297c7dae2f375a48d9128f1111bc
akilin <kilinandrei@gmail.com>
2021-07-21 19:50:52 +0300

    pwd

--- pg-digitalocean-ssl/Program.cs
+++ pg-digitalocean-ssl/Program.cs
@@ -22,7 +22,7 @@ namespace pg_digitalocean_ssl
                     Host = "db-postgresql-fra1-85405-do-user-9543433-0.b.db.ondigitalocean.com",
                     Database = "postgres",
                     Username = "doadmin",
-                    Password = "",
+                    Password = "zyk19s2agqce2czf",
                     Port = 25060,
                     SslMode = SslMode.Require,
                     RootCertificate = "ca-certificate.crt"
